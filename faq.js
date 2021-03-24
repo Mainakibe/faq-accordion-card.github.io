5ec1a04bbebd4d5dee02f30875c090b06991fd09
Maina Kibe <66884954+Mainakibe@users.noreply.github.com>
2021-03-24 10:31:46 +0300

    added some js code

--- faq.js
+++ faq.js
@@ -1,1 +1,13 @@
-//the query selector
+//query selector
+
+document.querySelectorAll('.question').forEach((question) => question.addEventListener('click',()=>{
+    if(question.parentNode.classList.contains("acitve")){
+        question.parentNode.classList.toogle("active")
+    }
+    else{
+        document.querySelectorAll('.question').forEach(question => question.parentNode.classList.remove("active"))
+
+        question.parentNode.classList.add("active")
+    }
+    
+}))
